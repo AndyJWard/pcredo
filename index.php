a3312997d461527a4f9e8590cb8d97ccc765a9e2
Andy Ward <andyward.eu@gmail.com>
2014-04-08 20:27:19 +0100

    index - insert pwd field link to DataChange

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ ob_start();
 
 	echo "<table><colgroup><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 75%;\"></colgroup>";
 	
-	echo "<tr><td><input type=\"text\" name=\"pwd\" border=\"0\"</td></tr>";
+	echo "<tr><td><input type=\"text\" name=\"pwd\" class=\"inp_no_border\"</td></tr>";
 	while($row=mysql_fetch_array($results))
 	{
 		$rdat = strtotime($row['wrelease']->createdate);
