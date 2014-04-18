6d225487bbe104aaf8ddc34ff97e5ab1d19aee6c
Andy Ward <andyward.eu@gmail.com>
2014-04-18 19:21:08 +0100

    change php default timezone - sorted

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ ob_start();
 	$qry1 = "SELECT wsubject, DATE_FORMAT(wrelease, '%d %b %Y') AS rdat, wid from weeks";
 	$qry2 = " WHERE DATE_FORMAT(wrelease, '%Y%m%d%k%i') <= " . date("YmdHi") . " ORDER BY wrelease DESC";
 
-echo $qry1 . $qry2;
+// echo $qry1 . $qry2;
 
 	$results = mysql_query($qry1.$qry2) or die(mysql_error());
 	
