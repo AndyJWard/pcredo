9c0581d53a501f99130919e6b78b455078a7101e
Andy Ward <andyward.eu@gmail.com>
2014-05-13 12:41:15 +0100

    result capture

---
+++ MyResults.php
@@ -0,0 +1,59 @@
+<?php 	
+header('Content-Type: text/html; charset=utf-8');
+ob_start();
+?>
+<head>
+<!--[if lt IE 9]>
+<script src="html5shiv.js"></script>
+<![endif]-->
+</head>
+<header>
+<link rel="stylesheet" type="text/css" href="post-credo.css">
+<title>Save Results</title>
+</header>
+
+<?php
+$pid = htmlspecialchars($_GET["Pid"]);
+$id = htmlspecialchars($_GET["Question"]);
+
+if (isset($_POST['cancel'])) {
+	echo '<meta http-equiv="refresh" content="0;URL=Preferences.php?Pid=' . $pid . '">';	
+}
+if (isset($_POST['save'])){
+		
+	define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
+	define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
+	define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
+	define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+
+	mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
+
+	mysql_select_db(DB_DATABASE) or die(mysql_error());
+
+	$query = 'SELECT * FROM results WHERE Pid=' . $pid
+
+	mysql_query($query);
+
+	echo "<table width=\"1200\">";
+
+	$res = mysql_query('SELECT * FROM results WHERE Pid=' . $pid . " ORDER BY Wid, Qid");
+
+	while ($row = mysql_fetch_array($res))
+		{
+		echo "<tr><td class=\"bk\" style=\"width:4%\">" . $row['Wid'] . "</td>";
+		echo "<td class=\"bk\" style=\"width:80%\">" . $row['Qid'] . "</td></tr>";
+		echo "<tr><td></td><td class=\"bl\">" . $row['Result'] . "</td></tr>";
+		}
+
+	echo "</table>";
+
+	mysql_close();
+	
+//foreach ($_POST as $key => $value)
+// echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
+		
+}
+?>
+	
+	
+	
