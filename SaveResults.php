ce5dd46a5c01e56b67e3deba00497ff2b9b671ae
Andy Ward <andyward.eu@gmail.com>
2014-05-13 09:05:46 +0100

    result capture

--- SaveResults.php
+++ SaveResults.php
@@ -105,8 +105,22 @@ mysql_query($query);
 
 	mysql_close();
 
+echo "<nav>";
+echo "<table width=\"800\"><tr align=\"left\" style=\"font-size: 12; color: black;\">";
+echo "<tr></tr>";
+echo '<td width="20%" class="bk90i">Results saved - where to now?</td>';
+echo '<td width="30%" class="bk90i"><a href="ViewResults.php?Pid=' . $pid . '&Question=' . $id . '">View the recorded results</a></td>';
+echo '<td width="30%" class="bk90i"><a href="Preferences.php?Pid=' . $pid . '&Question=' . $id . '">Change your recording preferences</a></td>';
+echo '<td width="20%" class="bk90i"><a href="index.php">Home</a></td>';
+echo "<tr></tr></table>";
+echo "</nav>";
+
+
 //foreach ($_POST as $key => $value)
 // echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
+		
+		
+		
 		
 }
 ?>
