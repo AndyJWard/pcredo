98ef128bdba5a6915480c1e50cb8a09e04da3256
Andy Ward <andyward.eu@gmail.com>
2014-06-01 17:12:25 +0100

    switching to

---
+++ PostSubmit.php
@@ -0,0 +1,56 @@
+<?php 	
+header('Content-Type: text/html; charset=utf-8');
+ob_start();
+?>
+<head>
+<!--[if lt IE 9]>
+<script src="html5shiv.js"></script>
+<![endif]-->
+</head>
+<header>
+<link rel="stylesheet" type="text/css" href="post-credo.css">
+<title>Save Preferences</title>
+</header>
+
+<?php
+
+
+	$url =  'Preferences.php';
+	$pars = 'Pid=' .$Pidy . 'Qid=' . $Qidy;
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST,1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $pars);
+	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt( $ch, CURLOPT_HEADER, 0);
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+//	$response = curl_exec( $ch );
+	curl_close($ch);
+	return false;
+
+
+
+
+$pid = htmlspecialchars($_GET["Pid"]);
+
+if (isset($_POST['cancel'])) {
+	echo '<meta http-equiv="refresh" content="0;URL=Preferences.php?Pid=' . $pid . '">';	
+}
+//if (isset($_POST['save'])){
+		
+	define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
+	define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
+	define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
+	define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+
+//	$id = htmlspecialchars($_GET["Question"]);
+	
+
+
+ foreach ($_POST as $key => $value)
+  echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
+		
+//}
+?>
+	
+	
+	
