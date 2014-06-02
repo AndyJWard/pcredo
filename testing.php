77850fb2ae62b55b48b722d8cc77009392c42ac4
Andy Ward <andyward.eu@gmail.com>
2014-06-02 22:26:20 +0100

    switching to

--- testing.php
+++ testing.php
@@ -13,6 +13,16 @@ ob_start();
 </header>
 
 <?php
+
+	define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
+	define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
+	define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
+	define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+
+	mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
+
+	mysql_select_db(DB_DATABASE) or die(mysql_error());
+	
 	echo '<nav>';
 	echo '<table width="800"><tr align="left" style="font-size: 12; color: black;">';
 	echo '<tr></tr>';
