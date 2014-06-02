d35450b1b1c35f3baae9c2f20efa152ae13acb4d
Andy Ward <andyward.eu@gmail.com>
2014-06-02 23:29:13 +0100

    switching to

--- testing.php
+++ testing.php
@@ -27,7 +27,7 @@ ob_start();
 	echo '<table width="800"><tr align="left" style="font-size: 12; color: black;">';
 	echo '<tr></tr>';
  	echo '<td width="40%" align="center">';
- 	echo '<select style="width: 170px;" id="WHO" size="1" onchange="myRedirect">';
+ 	echo '<select style="width: 170px;" id="WHO" size="1" onchange="redirectUrl">';
  	echo '<option value="0">Record Your Score';
 	$per_res = mysql_query("SELECT * FROM persons ORDER BY Pname");
 // find the names for the select field (to save results);
@@ -44,7 +44,7 @@ ob_start();
 ?>
 <script type="text/javascript" >
 
-var myRedirect = function(redirectUrl) {
+function(redirectUrl) {
 var form = $('<form action="http://pcredo-fridayquiz.rhcloud.com/ViewResults.php" method="post">' +
 '<input type="hidden" name="Pid" value="12" />' +
 '</form>');
