1414699443e35c8dee5960d479ad04e1180bebb6
Andy Ward <andyward.eu@gmail.com>
2014-10-29 18:52:59 +0000

    recover questions

--- ImportJsonFile.php
+++ ImportJsonFile.php
@@ -47,17 +47,17 @@ ob_start();
 
 	// Read the file contents into a string variable,
 // and parse the string into a data structure
-$str_data = file_get_contents($FileHome . "/QDataR.js");
-$data = json_decode($str_data,true);
- echo $data."<br>";
-echo "Question 1 week 1?: ".$data["weeks"]["week"]["questions"]["q"][0]."<br>";
-echo "Question 1 week 1?: ".$data["weeks"]["week"]["questions"]["q"][1]."<br>";
+//$str_data = file_get_contents($FileHome . "/QDataR.js");
+//$data = json_decode($str_data,true);
+// echo $data."<br>";
+//echo "Question 1 week 1?: ".$data["weeks"]["week"]["questions"]["q"][0]."<br>";
+//echo "Question 1 week 1?: ".$data["weeks"]["week"]["questions"]["q"][1]."<br>";
 echo "hello";
-echo "Weeks 0: " . $data["weeks"][0] . "<br>";
-echo "Weeks 1: " . $data["weeks"][1] . "<br>"; 
-echo var_dump(json_decode($data, $assoc = null);)
+//echo "Weeks 0: " . $data["weeks"][0] . "<br>";
+//echo "Weeks 1: " . $data["weeks"][1] . "<br>"; 
+//echo var_dump(json_decode($data, $assoc = null);)
 
-	mysql_close();
+//	mysql_close();
 
 
 ?>
