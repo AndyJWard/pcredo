261736fc73ebf62ffd95c5ca827233769f6d8b8a
Andy Ward <andy@andyward.eu>
2016-11-28 12:42:54 +0000

    searching

--- SearchResults.php
+++ SearchResults.php
@@ -48,15 +48,33 @@ ob_start();
 	echo "<table><colgroup><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 75%;\"></colgroup>";
 //	<p>My mother has <span style="color:blue">blue</span> eyes.</p>
 	echo "<tr><td></td><td class=\"td.bl90i\">Showing results for <span class=\"td.bl\"> " . $_POST['srch'] . "</span></td></tr>";
-		
+	$lastwid = "";	
+	$nums="";
 	while($row=mysql_fetch_array($results))
 	{
-		$rdat = strtotime($row['wrelease']->createdate);
-		
+//		$rdat = strtotime($row['wrelease']->createdate);
 
-		echo "<tr><td class=\"index_left\">" . $row['rdat'] . "</td>";
-		echo "<td class=\"index_right\"><a href=\"Question.php?question=" . $row['wid'] . "\"> " . $row['wsubject'] . "</a></td>";
-		echo "<td class=\"index_left\">Question " . $row['qnum'] . "</td></tr>";
+		if $lastwid==$row['wid'] {
+		
+			$nums = $nums . ", " . $row['qnum'];
+		
+		} else {
+		
+			if $nums==""	{
+	
+				$nums = $row['qnum'];
+			
+			}	
+		
+			echo "<tr><td class=\"index_left\">" . $row['rdat'] . "</td>";
+			echo "<td class=\"index_right\"><a href=\"Question.php?question=" . $row['wid'] . "\"> " . $row['wsubject'] . "</a></td>";	
+			echo "<td class=\"index_left\">Question " . $nums . "</td></tr>";	
+			echo "</table>";
+
+			$lastwid = $row['wid'];
+			$nums = "";
+		
+		}
 
 	}
 		echo "<tr></tr><tr><td class=\"td.t-link\"><a href=\"index.php\">Home</a></td><td class=\"td.t-link\"><a href=\"SearchFor.php\">New Search</a></td></tr>";	
