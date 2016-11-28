2c90dcb28845928640969bc3c1bd8a1f539c82df
Andy Ward <andy@andyward.eu>
2016-11-28 14:47:47 +0000

    searching

--- SearchResults.php
+++ SearchResults.php
@@ -48,7 +48,7 @@ ob_start();
 	
 	echo "<table><colgroup><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 50%;\"><col span=\"1\" style=\"width=: 25%;\"></colgroup>";
 
-	echo "<tr><td></td><td class=\"td.bl90i\">Showing results for <span class=\"td.bl\"> " . $_POST['srch'] . "</span></td></tr>";
+	echo "<tr><td></td><td class=\"td.bl90i\">Showing results for <span class=\"td.r120\"> " . $_POST['srch'] . "</span></td></tr>";
 
 	$lastwid = "";	
 	$nums = "";
