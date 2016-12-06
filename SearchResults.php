e00e9d08f03bc0f5dc0235a02a1f481bbdd55340
Andy Ward <andy@andyward.eu>
2016-12-06 23:29:07 +0000

    searching

--- SearchResults.php
+++ SearchResults.php
@@ -48,21 +48,16 @@ ob_start();
 	$results = mysql_query($qry1) or die(mysql_error());
 
 	$qct = 0;
-//	$qwid = array();
-//	$qnum = array();
-//	$qrdat = array();
-//	$qsubj = array();
-	
 	$q= array();	
-	
 	while($row=mysql_fetch_array($results)) {
-
 		$q[$qct++]=array($row['wid'],$row['qnum'],$row['rdat'],$row['wsubject']);
-
 	}
+
 
 echo "query of questions done";
 echo "array $q created [0][0] = " . $q[0][0] . " /[0][1] = " . $q[0][1] . " /[1][0] = " . $q[1][0] . " /[1][1] = " . $q[1][1];   
+
+
 
 	$srchstrg = "%" . $_POST["srchA"] . "%";
 
@@ -72,53 +67,81 @@ echo "array $q created [0][0] = " . $q[0][0] . " /[0][1] = " . $q[0][1] . " /[1]
 	$results = mysql_query($qry1) or die(mysql_error());
 
 	$act = 0;
-//	$awid = array();
-//	$anum = array();
-//	$ardat = array();
-//	$asubj = array();
-	
 	$a= array();	
-		
 	while($row=mysql_fetch_array($results)) {
-//		$awid[$act] = $row['wid'];
-//		$anum[$act] = $row['qnum'];
-//		$ardat[$act] = $row['rdat'];
-//		$asubj[$act++] = $row['wsubject'];			// includes increment of $act
-
 		$a[$act++]=array($row['wid'],$row['qnum'],$row['rdat'],$row['wsubject']);
-
 	}
+
 
 echo "query of answers done";
 echo "array $a created [0][0] = " . $a[0][0] . " /[0][1] = " . $a[0][1] . " /[1][0] = " . $a[1][0] . " /[1][1] = " . $a[1][1];   
 	
 	
-	mysql_close();
 	
+	mysql_close();
+
+	
+//  now put multiple questions (or answers) for a single week into a single line)
+$wid=0;
+$num=1;
+$dat=2;
+$subj=3;
+
+$ct=1;			// start off pointing at the second entry (and refer backwards)) 
+
+while ($ct<=$qct) {
+// echo " ct is " . $ct . "   :";
+//	if ($x[$ct][$wid]==$lastwid) {
+	if ($q[$ct][$wid]==$q[$ct-1][$wid]) {
+// echo " ct in if is " . $ct . "   :";
+		$q[$ct-1][$num] .= ", " . $q[$ct][$num];		// append this number to previous question's entry
+		array_splice($q,$ct,1);								// and get rid of this entry - index is automatically adjusted
+		$qct--;													// decrement input count to allow for removed element
+	}
+
+	$ct++;
+
+}
+
+
+$ct=1;			// start off pointing at the second entry (and refer backwards)) 
+
+while ($ct<=$act) {
+// echo " ct is " . $ct . "   :";
+//	if ($x[$ct][$wid]==$lastwid) {
+	if ($a[$ct][$wid]==$a[$ct-1][$wid]) {
+// echo " ct in if is " . $ct . "   :";
+		$a[$ct-1][$num] .= ", " . $a[$ct][$num];		// append this number to previous answer's entry
+		array_splice($a,$ct,1);								// and get rid of this entry - index is automatically adjusted
+		$act--;													// decrement input count to allow for removed element
+	}
+
+	$ct++;
+
+}
+
+
 
 
 	echo nl2br("<span STYLE='font-size:100%; font-style:italic; color:black'>Showing results from questions for </span><span  STYLE='font-size:130%; font-style:italic; color:blue'> " . $_POST['srchQ'] . "\n</span>");
 	
 	echo "<table><colgroup><col span=\"1\" style=\"width=: 15%;\"><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 10%;\"><col span=\"1\" style=\"width=: 15%;\"><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 10%;\"></colgroup>";
 
+// choos how long the 'while' should run for
+$whct = $qct;
+if($act>$qct) {
+	$whct = $act;
+}
 
-	$qlastwid = "";	
-	$qnums = "";
-
-	$alastwid = "";	
-	$anums = "";
-
-	$ctout=0;	
+while($loop++ <= $whct) {
+		
+		echo "<tr><td class=\"index_left\">" . $q[$whct][$dat] . "</td>";
+		echo "<td class=\"index_right\"><a href=\"Question.php?question=" . $q[$whct][$wid] . "\"> " . $q[$whct][$subj] . "</a></td>";	
+		echo "<td class=\"index_left\">Q " . $q[$whct][$num] . "</td></tr>";	
 	
-
+// question output, next do answer (on same line)
 	
-	
-		echo "<tr><td class=\"index_left\">" . $rdat . "</td>";
-		echo "<td class=\"index_right\"><a href=\"Question.php?question=" . $qlastwid . "\"> " . $subj . "</a></td>";	
-		echo "<td class=\"index_left\">Q " . $qnums . "</td></tr>";	
-	
-// finished ALL questions here	
-	
+}
 	
 		echo "<tr></tr><tr><td class=\"td.t-link\"><a href=\"index.php\">Home</a></td><td class=\"td.t-link\"><a href=\"SearchFor.php\">New Search</a></td></tr>";	
 		echo "</table>";
