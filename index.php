2f0f9ec6abd4a181a4ef9ce1bf8c1c1ce6496ece
Andy Ward <andy@andyward.eu>
2017-01-11 14:03:00 +0000

    test env

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php 
+<?php 	
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('Europe/London'); 
 ob_start();
+ini_set('display_errors', 1);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
@@ -29,40 +30,55 @@ ob_start();
 
 <?php
 
+$loc = $_SERVER["HTTP_HOST"];
+
+if($loc=="localhost"){
+
+	define( "DB_SERVER",'localhost' );
+	define( "DB_USER",'root');
+	define( "DB_PASSWORD",'');
+	define( "DB_DATABASE",'pcredo');
+
+} else {
+
 	define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
 	define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
 	define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
 	define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
+}
+
+	$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
+	mysqli_select_db($con, DB_DATABASE);
 
 // echo "Post defines <br/>";
 
-	mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
-
-	mysql_select_db(DB_DATABASE) or die(mysql_error());
-
-	$qry1 = "SELECT wsubject, DATE_FORMAT(wrelease, '%d %b %Y') AS rdat, wid from weeks";
+	$qry1 = "SELECT wsubject, wrelease, DATE_FORMAT(wrelease, '%d %b %Y') AS rdat, wid from weeks";
 	$qry2 = " WHERE DATE_FORMAT(wrelease, '%Y%m%d%k%i') <= " . date("YmdHi") . " ORDER BY wrelease DESC";
 
 // echo $qry1 . $qry2;
 
-	$results = mysql_query($qry1.$qry2) or die(mysql_error());
+
+	$results = mysqli_query($con, $qry1.$qry2) or die(mysqli_error($con));
 	
 	echo "<form action=\"DataChange.php\" method=\"post\">";
 
 	echo "<table><colgroup><col span=\"1\" style=\"width=: 25%;\"><col span=\"1\" style=\"width=: 75%;\"></colgroup>";
 		
-	while($row=mysql_fetch_array($results))
+	while($row=mysqli_fetch_array($results))
 	{
-		$rdat = strtotime($row['wrelease']->createdate);
-		
-
+		$rdat = strtotime($row['wrelease']);
+//		$rdat = strtotime($row['wrelease']->createdate);
 		echo "<tr><td class=\"index_left\">" . $row['rdat'] . "</td>";
 		echo "<td class=\"index_right\"><a href=\"Question.php?question=" . $row['wid'] . "\"> " . $row['wsubject'] . "</a></td></tr>";
 
 	}
 	echo "<tr><td><input type=\"password\" name=\"pwd\" style></td></tr>";	
-		echo "</table>";
-mysql_close();
+	echo "</table>";
+		
+		// Free result set
+	mysqli_free_result($results);
+	
+	mysqli_close();
 
 ?>
 
