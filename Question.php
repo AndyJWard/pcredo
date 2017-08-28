c95627e13b65e42236ab2fba9b5e9bcaadba03f3
Andy Ward <andy@andyward.eu>
2017-08-28 18:16:44 +0100

    Update to mysqli and use require of db.php

--- Question.php
+++ Question.php
@@ -15,18 +15,8 @@ session_start();
 <body>
 
 <?php
-	define( "DB_SERVER",    getenv('OPENSHIFT_MYSQL_DB_HOST') );
-	define( "DB_USER",      getenv('OPENSHIFT_MYSQL_DB_USERNAME') );
-	define( "DB_PASSWORD",  getenv('OPENSHIFT_MYSQL_DB_PASSWORD') );
-	define( "DB_DATABASE",  getenv('OPENSHIFT_APP_NAME') );
 
-// echo "Post defines <br/>";
-
-	mysql_connect(DB_SERVER,DB_USER,DB_PASSWORD) or die(mysql_error());
-
-// echo "Post connect server:" . DB_SERVER . "  user:" . DB_USER . "<br/>";
-	
-	mysql_select_db(DB_DATABASE) or die(mysql_error());
+require('db.php');
 
 // echo "Post select_db <br/>";
 
@@ -35,17 +25,17 @@ session_start();
 
 	$ans = htmlspecialchars($_GET["reveal"]);	// reveal=Y or reveal=N
 
-	$wk_res = mysql_query("SELECT  DATE_FORMAT(wrelease, '%d %b %Y') as rdate, wsubject, wcomment FROM weeks WHERE wid = " . $id );
-
-	$wk_row = mysql_fetch_array($wk_res);
+	$wk_res = mysqli_query($con, "SELECT  DATE_FORMAT(wrelease, '%d %b %Y') as rdate, wsubject, wcomment FROM weeks WHERE wid = " . $id );
+	
+	$wk_row = mysqli_fetch_array($wk_res);
 
 	echo "<table><tr><td class=\"bl\">" . $wk_row['rdate'] . " - " . $wk_row['wsubject'] . "</td>";
 
 	echo "<tr><td class=\"bk90i\">" . $wk_row['wcomment'] . "</td></tr></table><table>";
 
-	$res = mysql_query("SELECT * FROM questions WHERE qwid = " . $id . " ORDER BY qnum");
+	$res = mysqli_query($con, "SELECT * FROM questions WHERE qwid = " . $id . " ORDER BY qnum");
 
-	while ($row = mysql_fetch_array($res))
+	while ($row = mysqli_fetch_array($res))
 		{
 		echo "<tr><td class=\"bk\" size=\"2\">" . $row['qnum'] . "</td>";
 		echo "<td class=\"bk\" size=\"100\">" . $row['qquestion'] . "</td></tr>";
@@ -57,7 +47,7 @@ session_start();
 
 	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=Answer.php?question=" . $id . ">Answers</a>";
 	
-	mysql_close();
+	mysqli_close();
 
 ?>
 </body>
